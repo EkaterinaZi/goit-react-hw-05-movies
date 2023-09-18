5d3dc59842b348edd2350c496f8aaf1fb3268e54
EkaterinaZi <kat.zinovik@gmail.com>
2023-09-18 10:10:06 +0100

    update

--- src/pages/Movie/Movies.js
+++ src/pages/Movie/Movies.js
@@ -15,8 +15,7 @@ useEffect(() => {
         setMovies([]);
         return 
       }
-      fetchSearchMovies(query)
-      .then((data => setMovies([...data.results])))  
+      
 },[query]);
 
 const handleChange = e => {
