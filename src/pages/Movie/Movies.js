f9e4ccd265e877f69588d88e0549d9933995817e
EkaterinaZi <kat.zinovik@gmail.com>
2023-09-19 10:47:46 +0100

    update

--- src/pages/Movie/Movies.js
+++ src/pages/Movie/Movies.js
@@ -1,59 +1,70 @@
 import { useState, useEffect } from "react";
 import { fetchSearchMovies } from "components/Utils/Fetch";
-import { Link, useLocation, useSearchParams} from "react-router-dom"
+import {Link, useLocation, useSearchParams} from "react-router-dom"
 import css from './Movie.module.css'
 const Movies = () =>{ 
 const [searchParams, setSearchParams] =useSearchParams();
 const query = searchParams.get('query') ?? "";
-const [movies, setMovies] = useState([])
+const [movies, setMovies] = useState([]);
+const [inputQuery, setInputQuery] = useState([])
 const location = useLocation();
 const [loading, setLoading] = useState(false);
 
 
+
 useEffect(() => {
-    if (!query) {
-        setMovies([]);
-        return 
+    console.log(query)
+      if(query){
+      fetchSearchMovies(query)
+      .then((data => setMovies([...data.results])))  
       }
-      
+      return setMovies([]);
 },[query]);
 
 const handleChange = e => {
-    if(e.currentTarget.value === ""){ return setSearchParams({})}
-    setSearchParams ({query: e.currentTarget.value}) ;
+    const {value} = e.target;
+    setInputQuery(value);
     }
+
 
 const handleSubmit = async e => {
     e.preventDefault();
-    if(query.trim() === ''){
-        
-        alert('Enter data')
+    if(inputQuery.trim() === ''){
+         alert('Enter data')
+         setMovies([])
+         setSearchParams('')
         return 
     } 
+    setSearchParams ({query: inputQuery});
     try{
             const data = await  fetchSearchMovies(query)
             setMovies([...data.results])
         }
      catch (error){}
      finally {
-        setLoading(false);
+       setLoading(false);
       }
+     
     }
- 
 
     return(
         <>
        <h2 className={css.title}>Movies</h2>
-       <form onSubmit={handleSubmit}>
+       <form 
+       onSubmit={handleSubmit}
+       >
         <label> Enter data
-            <input type="text" onChange={handleChange}
-            value={query} ></input>
+            <input type="text" name="query" onChange={handleChange}
+               autoComplete="off"
+               autoFocus
+               placeholder="Search Movies"
+               value={inputQuery}></input>
         </label>
         <button type="submit"  >Find</button>
        </form>
+       {loading && <div>Loading...</div>}
        <ul>
-        {loading && <div>Loading...</div>}
-      {movies.map(({id, title}) => {
+       {movies.map(({id, title}) => {
         return ( 
         <li key={id}>
             <Link to={`${id}`} state={{from: location}} className={css.movie}>{title}</Link>
@@ -67,3 +78,4 @@ const handleSubmit = async e => {
     }
     
     export default Movies;
+
